b048d844cf3f9d9ee54d0c33010674ddbe0045b7
gourab-stu <gourab.acharjee2005@gmail.com>
2023-12-22 23:54:42 +0530

    Create BinarySearch.cpp

---
+++ DS/BKP sir/searching/BinarySearch.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+
+int arr[100];
+int size;
+
+void input() {
+    std::cout << "Enter the size of the array: ";
+    std::cin >> size;
+    for (int i = 0; i < size; i++) {
+        std::cout << "Enter value no " << (i + 1) << ": ";
+        std::cin >> arr[i];
+    }
+}
+
+void binarySearch(int searchValue) {
+    int low = 0, high = size - 1, mid;
+    while (low <= high) {
+        mid = (low + high) / 2;
+        if (arr[mid] == searchValue) {
+            std::cout << "Search value found "<< std::endl;
+            return;
+        } else {
+            if (searchValue < arr[mid])
+                high = mid - 1;
+            else
+                low = mid + 1;
+        }
+    }
+    std::cout << "Search value not found" << std::endl;
+}
+
+int main() {
+    int value;
+    input();
+    std::cout << "Enter the value to search: ";
+    std::cin >> value;
+    binarySearch(value);
+    return 0;
+}
