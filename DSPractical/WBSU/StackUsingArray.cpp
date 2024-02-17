531a2ab463c669538875277ba0a65181b3a1212f
gourab-stu <gourab.acharjee2005@gmail.com>
2024-02-17 22:30:01 +0530

    updated .gitignore and pushed NS2 tcl files

---
+++ DSPractical/WBSU/StackUsingArray.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+
+int stack[100], n=100, top=-1;
+
+void push(int val) {
+   if(top>=n-1)
+   std::cout<<"Stack Overflow"<<std::endl;
+   else {
+      top++;
+      stack[top]=val;
+   }
+}
+
+void pop() {
+   if(top<=-1)
+   std::cout<<"Stack Underflow"<<std::endl;
+   else {
+      std::cout<<"The popped element is "<< stack[top] <<std::endl;
+      top--;
+   }
+}
+
+void display() {
+   if(top>=0) {
+      std::cout<<"Stack elements are:";
+      for(int i=top; i>=0; i--)
+      std::cout<<stack[i]<<" ";
+      std::cout<<std::endl;
+   } else
+   std::cout<<"Stack is empty";
+}
+
+int main() {
+   int ch, val;
+   std::cout<<"1) Push in stack"<<std::endl;
+   std::cout<<"2) Pop from stack"<<std::endl;
+   std::cout<<"3) Display stack"<<std::endl;
+   std::cout<<"4) Exit"<<std::endl;
+   do {
+      std::cout<<"Enter choice: "<<std::endl;
+      std::cin>>ch;
+      switch(ch) {
+         case 1: {
+            std::cout<<"Enter value to be pushed:"<<std::endl;
+            std::cin>>val;
+            push(val);
+            break;
+         }
+         case 2: {
+            pop();
+            break;
+         }
+         case 3: {
+            display();
+            break;
+         }
+         case 4: {
+            std::cout<<"Exit"<<std::endl;
+            break;
+         }
+         default: {
+            std::cout<<"Invalid Choice"<<std::endl;
+         }
+      }
+   }while(ch!=4);
+   return 0;
+}
---
+++ DSPractical/WBSU/StackUsingLinkedList.cpp
@@ -0,0 +1,73 @@
+#include<iostream>
+
+struct node {
+    int data;
+    struct node *link;
+};
+
+struct node *top = NULL, *temp;
+
+void push() {
+    temp = new node;
+    if (temp == NULL)
+        std::cout << "Memory cannot be allocated\n";
+    else {
+        std::cout << "Enter data to push: ";
+        std::cin >> temp->data;
+        temp->link = top;
+        std::cout << "Value pushed";
+        top = temp;
+    }
+}
+
+void pop() {
+    if (pop == NULL)
+        std::cout << "Stack Underflow";
+    else {
+        temp = top;
+        top = top->link;
+        std::cout << "Popped value = " << temp->data;
+        free(temp);
+    }
+}
+
+void display() {
+    temp = top;
+    if (temp == NULL)
+        std::cout << "Empty Stack";
+    else {
+        while (temp != NULL) {
+            std::cout << temp->data << " ";
+            temp = temp->link;
+        }
+    }
+}
+
+int main() {
+    std::cout << "\t1. push element\n\t2. pop element\n\t3. display stack\n\t4. exit\n";
+    while(1) {
+        std::cout << "\nEnter choice : ";
+        int choice;
+        std::cin >> choice;
+        switch (choice) {
+        case 1:
+            push();
+            break;
+        case 2:
+            pop();
+            break;
+        case 3:
+            std::cout << "\nThe Stack is\n";
+            display();
+            break;
+        case 4:
+            std::cout << "\nEnd of program\n";
+            break;
+        default:
+            std::cout << "\nInvalid input\n";
+        }
+        if(choice == 4)
+            break;
+    }
+    return 0;
+}
