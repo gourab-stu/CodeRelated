a76615bfe41940724dabae9ea55ce0e83f4b6c7d
Gourab Acharjee <gourab.acharjee2005@gmail.com>
2024-02-23 14:37:39 +0530

    updated sparse matrix

--- DSPractical/WBSU/SparseMatrix.cpp
+++ DSPractical/WBSU/SparseMatrix.cpp
@@ -1,45 +1,62 @@
 #include<iostream>
 
-int main() {
-    int a[10][10], b[10][3], row, col, count = 0, i, j, k = 1;
-    std::cout << "Enter total number of rows: ";
-    std::cin >> row;
-    std::cout << "Enter total number of columns: ";
-    std::cin >> col;
-    for(i = 0; i < row; i++) {
-        for(j = 0; j < col; j++) {
-            std::cout << "a[" << i << "][" << j << "] = ";
-            std::cin >> a[i][j];
-        }
-    }
-    std::cout << "The matrix is\n";
-    for(i = 0; i < row; i++) {
-        for(j = 0; j < col; j++)
-            std::cout << a[i][j] << "\t";
-        std::cout << "\n";
-    }
-    for(i = 0; i < row; i++)
-        for(j = 0; j < col; j++)
-            if(a[i][j] != 0)
-                count++;
-    b[0][0] = row;
-    b[0][1] = col;
-    b[0][2] = count;
-    for(i = 0; i < row; i++) {
-        for(j = 0; j < col; j++) {
-            if(a[i][j] != 0) {
-                b[k][0] = i;
-                b[k][1] = j;
-                b[k][2] = a[i][j];
-                k++;
+class Matrix {
+    int row, col, count = 0, i, j, k = 1;
+public:
+    int  a[10][10], b[10][10];
+    // takes the inputs of the matrix
+    void input() {
+        std::cout << "Enter total number of rows: ";
+        std::cin >> row;
+        std::cout << "Enter total number of columns: ";
+        std::cin >> col;
+        for(i = 0; i < row; i++) {
+            for(j = 0; j < col; j++) {
+                std::cout << "a[" << i << "][" << j << "] = ";
+                std::cin >> a[i][j];
             }
         }
     }
-    std::cout << "The output matrix is\n";
-    for(i = 0; i < (count + 1); i++) {
-        for(j = 0; j < 3; j++)
-            std::cout << b[i][j] << "\t";
-        std::cout << "\n";
+    // counts the total number of non-zero elements
+    void totalNonZero() {
+        for(i = 0; i < row; i++)
+            for(j = 0; j < col; j++)
+                if(a[i][j] != 0)
+                    count++;
     }
+    // converts the entire matrix into sparse matrix
+    void toSparseMatrix() {
+        b[0][0] = row;
+        b[0][1] = col;
+        b[0][2] = count;
+        for(i = 0; i < row; i++) {
+            for(j = 0; j < col; j++) {
+                if(a[i][j] != 0) {
+                    b[k][0] = i;
+                    b[k][1] = j;
+                    b[k][2] = a[i][j];
+                    k++;
+                }
+            }
+        }
+    }
+    // displays the matrix
+    void display(int arr[10][10]) {
+        std::cout << "The matrix is\n";
+        for(i = 0; i < (count + 1); i++) {
+            for(j = 0; j < 3; j++)
+                std::cout << arr[i][j] << "\t";
+            std::cout << "\n";
+        }
+    }
+};
+
+int main() {
+    Matrix ob;
+    ob.input();
+    ob.display(ob.a);
+    ob.toSparseMatrix();
+    std::cout << "After converting to sparse matrix\n";
+    ob.display(ob.b);
     return 0;
 }
