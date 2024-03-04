cc236f2b7e5bfe0b0421d505e899f3c0cdd8faf4
Gourab Acharjee <gourab.acharjee2005@gmail.com>
2024-03-04 13:56:34 +0530

    Updated JavaCode

--- JavaCode/practice/CountAllVowels.java
+++ JavaCode/practice/CountAllVowels.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class CountAllVowels {
     public static String input() {
         System.out.print("Enter the word: ");
@@ -7,10 +9,16 @@ public class CountAllVowels {
     }
 
     public static int countVowels(String word) {
-        int count;
+        int count = 0;
+        for(int i = 0; i < word.length(); i++) {
+            if((word.charAt(i) == 'a') || (word.charAt(i) == 'e') || (word.charAt(i) == 'i') || (word.charAt(i) == 'o') || (word.charAt(i) == 'u'))
+                count++;
+        }
+        return count;
     }
     
     public static void main(String[] args) {
         String word = input();
+        System.out.println("Number of vowels in word '" + word + "': " + countVowels(word));
     }
 }
--- JavaCode/practice/asciiValuesOfCharacter.java
+++ JavaCode/practice/asciiValuesOfCharacter.java
@@ -1,18 +1,16 @@
 import java.util.Scanner;
 
-public class asciiValuesOfCharacter
-{
-    public static void main(String[] args)
-    {
-        Scanner s=new Scanner(System.in);
+public class asciiValuesOfCharacter {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
         System.out.print("Enter a character: ");
-        char ch=s.next().charAt(0);
-        System.out.println("ASCII value of "+ch+" is "+(int)ch);
-        int i=ch;
-        System.out.println("ASCII value of "+ch+" is "+i);
+        char ch = s.next().charAt(0);
+        System.out.println("ASCII value of " + ch + " is " + (int) ch);
+        int i = ch;
+        System.out.println("ASCII value of " + ch + " is " + i);
         System.out.print("Enter an integer: ");
-        int a=s.nextInt();
-        System.out.println("Character having ASCII value "+a+" is "+(char)a);
+        int a = s.nextInt();
+        System.out.println("Character having ASCII value " + a + " is " + (char) a);
         s.close();
     }
 }
