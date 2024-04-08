e5161b9210eb0fe6a93ad6b2bd34cd9e209c8a46
Gourab Acharjee <gourab.acharjee2005@gmail.com>
2024-04-08 21:47:20 +0530

    deleted temporary

--- temporary/temp4.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-
-public class temp4 {
-    public static void main(String[] args) {
-        Scanner s=new Scanner(System.in);
-        System.out.println("Enter the word: ");
-        s.close();
-    }
-}
