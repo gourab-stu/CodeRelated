01a42eb4aec0f446fa8812828044c78663ca472b
gourab-stu <gourab.acharjee2005@gmail.com>
2024-09-05 16:54:38 +0530

    small changes

--- SQL/Oracle/day1q3.sql
+++ SQL/Oracle/day1q3.sql
@@ -21,87 +21,87 @@ CREATE TABLE manages (
     manager_name VARCHAR(30)
 );
 
-INSERT INTO employee (emp_name, street, city) VALUES ('Aarav', 'BT Road', 'Barrackpore');
-INSERT INTO employee (emp_name, street, city) VALUES ('Rajat', 'GT Road', 'Kolkata');
-INSERT INTO employee (emp_name, street, city) VALUES ('Vivaan', 'Jessore Road', 'Shyambazar');
-INSERT INTO employee (emp_name, street, city) VALUES ('Aditya', 'Barasat Road', 'Sodepur');
-INSERT INTO employee (emp_name, street, city) VALUES ('Arjun', 'AJC Bose Road', 'Maniktala');
-INSERT INTO employee (emp_name, street, city) VALUES ('Saima', 'MG Road', 'Mumbai');
-INSERT INTO employee (emp_name, street, city) VALUES ('Reyansh', 'GT Road', 'Kolkata');
-INSERT INTO employee (emp_name, street, city) VALUES ('Ayaan', 'BT Road', 'Barrackpore');
-INSERT INTO employee (emp_name, street, city) VALUES ('Krishna', 'MG Road', 'Mumbai');
-INSERT INTO employee (emp_name, street, city) VALUES ('Ishaan', 'GT Road', 'Kolkata');
-INSERT INTO employee (emp_name, street, city) VALUES ('Ananya', 'BT Road', 'Barrackpore');
-INSERT INTO employee (emp_name, street, city) VALUES ('Diya', 'Barasat Road', 'Sodepur');
-INSERT INTO employee (emp_name, street, city) VALUES ('Pari', 'AJC Bose Road', 'Maniktala');
-INSERT INTO employee (emp_name, street, city) VALUES ('Saanvi', 'Jessore Road', 'Shyambazar');
-INSERT INTO employee (emp_name, street, city) VALUES ('Aadhya', 'AJC Bose Road', 'Maniktala');
-INSERT INTO employee (emp_name, street, city) VALUES ('Anika', 'MG Road', 'Mumbai');
-INSERT INTO employee (emp_name, street, city) VALUES ('Anvi', 'BT Road', 'Barrackpore');
-INSERT INTO employee (emp_name, street, city) VALUES ('Aarohi', 'Jessore Road', 'Shyambazar');
-INSERT INTO employee (emp_name, street, city) VALUES ('Nisha', 'MG Road', 'Mumbai');
-INSERT INTO employee (emp_name, street, city) VALUES ('Sneha', 'AJC Bose Road', 'Maniktala');
+INSERT INTO employee VALUES ('Aarav', 'BT Road', 'Barrackpore');
+INSERT INTO employee VALUES ('Rajat', 'GT Road', 'Kolkata');
+INSERT INTO employee VALUES ('Vivaan', 'Jessore Road', 'Shyambazar');
+INSERT INTO employee VALUES ('Aditya', 'Barasat Road', 'Sodepur');
+INSERT INTO employee VALUES ('Arjun', 'AJC Bose Road', 'Maniktala');
+INSERT INTO employee VALUES ('Saima', 'MG Road', 'Mumbai');
+INSERT INTO employee VALUES ('Reyansh', 'GT Road', 'Kolkata');
+INSERT INTO employee VALUES ('Ayaan', 'BT Road', 'Barrackpore');
+INSERT INTO employee VALUES ('Krishna', 'MG Road', 'Mumbai');
+INSERT INTO employee VALUES ('Ishaan', 'GT Road', 'Kolkata');
+INSERT INTO employee VALUES ('Ananya', 'BT Road', 'Barrackpore');
+INSERT INTO employee VALUES ('Diya', 'Barasat Road', 'Sodepur');
+INSERT INTO employee VALUES ('Pari', 'AJC Bose Road', 'Maniktala');
+INSERT INTO employee VALUES ('Saanvi', 'Jessore Road', 'Shyambazar');
+INSERT INTO employee VALUES ('Aadhya', 'AJC Bose Road', 'Maniktala');
+INSERT INTO employee VALUES ('Anika', 'MG Road', 'Mumbai');
+INSERT INTO employee VALUES ('Anvi', 'BT Road', 'Barrackpore');
+INSERT INTO employee VALUES ('Aarohi', 'Jessore Road', 'Shyambazar');
+INSERT INTO employee VALUES ('Nisha', 'MG Road', 'Mumbai');
+INSERT INTO employee VALUES ('Sneha', 'AJC Bose Road', 'Maniktala');
 
 SELECT * FROM employee;
 
-INSERT INTO company (company_name, city) VALUES ('ABC', 'Kolkata');
-INSERT INTO company (company_name, city) VALUES ('ABC', 'Shyambazar');
-INSERT INTO company (company_name, city) VALUES ('ABC', 'Mumbai');
-INSERT INTO company (company_name, city) VALUES ('IOC', 'Barrackpore');
-INSERT INTO company (company_name, city) VALUES ('IOC', 'Kolkata');
-INSERT INTO company (company_name, city) VALUES ('IOC', 'Shyambazar');
-INSERT INTO company (company_name, city) VALUES ('IOC', 'Sodepur');
-INSERT INTO company (company_name, city) VALUES ('IOC', 'Maniktala');
-INSERT INTO company (company_name, city) VALUES ('IOC', 'Mumbai');
-INSERT INTO company (company_name, city) VALUES ('XYZ', 'Barrackpore');
-INSERT INTO company (company_name, city) VALUES ('XYZ', 'Kolkata');
-INSERT INTO company (company_name, city) VALUES ('XYZ', 'Mumbai');
+INSERT INTO company VALUES ('ABC', 'Kolkata');
+INSERT INTO company VALUES ('ABC', 'Shyambazar');
+INSERT INTO company VALUES ('ABC', 'Mumbai');
+INSERT INTO company VALUES ('IOC', 'Barrackpore');
+INSERT INTO company VALUES ('IOC', 'Kolkata');
+INSERT INTO company VALUES ('IOC', 'Shyambazar');
+INSERT INTO company VALUES ('IOC', 'Sodepur');
+INSERT INTO company VALUES ('IOC', 'Maniktala');
+INSERT INTO company VALUES ('IOC', 'Mumbai');
+INSERT INTO company VALUES ('XYZ', 'Barrackpore');
+INSERT INTO company VALUES ('XYZ', 'Kolkata');
+INSERT INTO company VALUES ('XYZ', 'Mumbai');
 
 SELECT * FROM company;
 
-INSERT INTO work (emp_name, company_name, salary) VALUES ('Aarav', 'IOC', 12000);
-INSERT INTO work (emp_name, company_name, salary) VALUES ('Rajat', 'ABC', 15000);
-INSERT INTO work (emp_name, company_name, salary) VALUES ('Vivaan', 'XYZ', 18000);
-INSERT INTO work (emp_name, company_name, salary) VALUES ('Aditya', 'IOC', 13000);
-INSERT INTO work (emp_name, company_name, salary) VALUES ('Arjun', 'ABC', 14000);
-INSERT INTO work (emp_name, company_name, salary) VALUES ('Saima', 'XYZ', 17000);
-INSERT INTO work (emp_name, company_name, salary) VALUES ('Reyansh', 'IOC', 16000);
-INSERT INTO work (emp_name, company_name, salary) VALUES ('Ayaan', 'ABC', 9000);
-INSERT INTO work (emp_name, company_name, salary) VALUES ('Krishna', 'XYZ', 19000);
-INSERT INTO work (emp_name, company_name, salary) VALUES ('Ishaan', 'IOC', 11000);
-INSERT INTO work (emp_name, company_name, salary) VALUES ('Ananya', 'ABC', 14000);
-INSERT INTO work (emp_name, company_name, salary) VALUES ('Diya', 'XYZ', 17000);
-INSERT INTO work (emp_name, company_name, salary) VALUES ('Pari', 'IOC', 13000);
-INSERT INTO work (emp_name, company_name, salary) VALUES ('Saanvi', 'ABC', 14000);
-INSERT INTO work (emp_name, company_name, salary) VALUES ('Aadhya', 'XYZ', 17000);
-INSERT INTO work (emp_name, company_name, salary) VALUES ('Anika', 'IOC', 15000);
-INSERT INTO work (emp_name, company_name, salary) VALUES ('Anvi', 'ABC', 10000);
-INSERT INTO work (emp_name, company_name, salary) VALUES ('Aarohi', 'XYZ', 16000);
-INSERT INTO work (emp_name, company_name, salary) VALUES ('Nisha', 'IOC', 12000);
-INSERT INTO work (emp_name, company_name, salary) VALUES ('Sneha', 'ABC', 15000);
+INSERT INTO work VALUES ('Aarav', 'IOC', 12000);
+INSERT INTO work VALUES ('Rajat', 'ABC', 15000);
+INSERT INTO work VALUES ('Vivaan', 'XYZ', 18000);
+INSERT INTO work VALUES ('Aditya', 'IOC', 13000);
+INSERT INTO work VALUES ('Arjun', 'ABC', 14000);
+INSERT INTO work VALUES ('Saima', 'XYZ', 17000);
+INSERT INTO work VALUES ('Reyansh', 'IOC', 16000);
+INSERT INTO work VALUES ('Ayaan', 'ABC', 9000);
+INSERT INTO work VALUES ('Krishna', 'XYZ', 19000);
+INSERT INTO work VALUES ('Ishaan', 'IOC', 11000);
+INSERT INTO work VALUES ('Ananya', 'ABC', 14000);
+INSERT INTO work VALUES ('Diya', 'XYZ', 17000);
+INSERT INTO work VALUES ('Pari', 'IOC', 13000);
+INSERT INTO work VALUES ('Saanvi', 'ABC', 14000);
+INSERT INTO work VALUES ('Aadhya', 'XYZ', 17000);
+INSERT INTO work VALUES ('Anika', 'IOC', 15000);
+INSERT INTO work VALUES ('Anvi', 'ABC', 10000);
+INSERT INTO work VALUES ('Aarohi', 'XYZ', 16000);
+INSERT INTO work VALUES ('Nisha', 'IOC', 12000);
+INSERT INTO work VALUES ('Sneha', 'ABC', 15000);
 
 SELECT * FROM work;
 
-INSERT INTO manages (emp_name, manager_name) VALUES ('Aarav', 'Rajat');
-INSERT INTO manages (emp_name, manager_name) VALUES ('Rajat', 'Rajat');
-INSERT INTO manages (emp_name, manager_name) VALUES ('Vivaan', 'Rajat');
-INSERT INTO manages (emp_name, manager_name) VALUES ('Aditya', 'Rajat');
-INSERT INTO manages (emp_name, manager_name) VALUES ('Arjun', 'Saima');
-INSERT INTO manages (emp_name, manager_name) VALUES ('Saima', 'Saima');
-INSERT INTO manages (emp_name, manager_name) VALUES ('Reyansh', 'Saima');
-INSERT INTO manages (emp_name, manager_name) VALUES ('Ayaan', 'Saima');
-INSERT INTO manages (emp_name, manager_name) VALUES ('Krishna', 'Saima');
-INSERT INTO manages (emp_name, manager_name) VALUES ('Ishaan', 'Ishaan');
-INSERT INTO manages (emp_name, manager_name) VALUES ('Ananya', 'Ishaan');
-INSERT INTO manages (emp_name, manager_name) VALUES ('Diya', 'Ishaan');
-INSERT INTO manages (emp_name, manager_name) VALUES ('Pari', 'Ishaan');
-INSERT INTO manages (emp_name, manager_name) VALUES ('Saanvi', 'Saanvi');
-INSERT INTO manages (emp_name, manager_name) VALUES ('Aadhya', 'Saanvi');
-INSERT INTO manages (emp_name, manager_name) VALUES ('Anika', 'Saanvi');
-INSERT INTO manages (emp_name, manager_name) VALUES ('Anvi', 'Saanvi');
-INSERT INTO manages (emp_name, manager_name) VALUES ('Aarohi', 'Saanvi');
-INSERT INTO manages (emp_name, manager_name) VALUES ('Nisha', 'Saanvi');
-INSERT INTO manages (emp_name, manager_name) VALUES ('Sneha', 'Saanvi');
+INSERT INTO manages VALUES ('Aarav', 'Rajat');
+INSERT INTO manages VALUES ('Rajat', 'Rajat');
+INSERT INTO manages VALUES ('Vivaan', 'Rajat');
+INSERT INTO manages VALUES ('Aditya', 'Rajat');
+INSERT INTO manages VALUES ('Arjun', 'Saima');
+INSERT INTO manages VALUES ('Saima', 'Saima');
+INSERT INTO manages VALUES ('Reyansh', 'Saima');
+INSERT INTO manages VALUES ('Ayaan', 'Saima');
+INSERT INTO manages VALUES ('Krishna', 'Saima');
+INSERT INTO manages VALUES ('Ishaan', 'Ishaan');
+INSERT INTO manages VALUES ('Ananya', 'Ishaan');
+INSERT INTO manages VALUES ('Diya', 'Ishaan');
+INSERT INTO manages VALUES ('Pari', 'Ishaan');
+INSERT INTO manages VALUES ('Saanvi', 'Saanvi');
+INSERT INTO manages VALUES ('Aadhya', 'Saanvi');
+INSERT INTO manages VALUES ('Anika', 'Saanvi');
+INSERT INTO manages VALUES ('Anvi', 'Saanvi');
+INSERT INTO manages VALUES ('Aarohi', 'Saanvi');
+INSERT INTO manages VALUES ('Nisha', 'Saanvi');
+INSERT INTO manages VALUES ('Sneha', 'Saanvi');
 
 SELECT * FROM manages;
 
@@ -125,17 +125,23 @@ WHERE company_name = 'IOC';
 -- q4 
 SELECT DISTINCT c1.company_name
 FROM company c1
-WHERE NOT EXISTS ((SELECT c2.city FROM company c2 WHERE c2.company_name = 'ABC') MINUS
-                  (SELECT c3.city FROM company c3 WHERE c3.company_name = c1.company_name));
+WHERE NOT EXISTS ((SELECT c2.city 
+                   FROM company c2 
+                   WHERE c2.company_name = 'ABC') MINUS
+                  (SELECT c3.city 
+                   FROM company c3 
+                   WHERE c3.company_name = c1.company_name));
 
 -- q4 ChatGPT
 SELECT DISTINCT company_name
 FROM company c1
 WHERE NOT EXISTS (SELECT city
                   FROM company c2
-                  WHERE c2.company_name = 'ABC' AND NOT EXISTS (SELECT *
-                                                                FROM Company c3
-                                                                WHERE c3.company_name = C1.company_name AND c3.city = c2.city));
+                  WHERE c2.company_name = 'ABC' AND 
+                        NOT EXISTS (SELECT *
+                                    FROM Company c3
+                                    WHERE c3.company_name = C1.company_name AND 
+                                          c3.city = c2.city));
 
 -- q5
 SELECT emp_name, city
--- SQL/Oracle/day3q1.sql
+++ SQL/Oracle/day3q1.sql
@@ -26,57 +26,57 @@ CREATE TABLE WorksOn (
     proj_code NUMBER REFERENCES Project
 );
 
-INSERT INTO Dept (dept_no, dept_name, dept_head) VALUES (1, 'sales', 'Mainak');
-INSERT INTO Dept (dept_no, dept_name, dept_head) VALUES (2, 'finance', 'Tuhin');
-INSERT INTO Dept (dept_no, dept_name, dept_head) VALUES (3, 'management', 'Santosh');
-INSERT INTO Dept (dept_no, dept_name, dept_head) VALUES (4, 'quality', 'Anjali');
+INSERT INTO Dept VALUES (1, 'sales', 'Mainak');
+INSERT INTO Dept VALUES (2, 'finance', 'Tuhin');
+INSERT INTO Dept VALUES (3, 'management', 'Santosh');
+INSERT INTO Dept VALUES (4, 'quality', 'Anjali');
 
 SELECT * FROM Dept;
 
-INSERT INTO Employee (emp_no, emp_name, dept, manager) VALUES (1, 'Mainak', 'sales', 'Bhoomi');
-INSERT INTO Employee (emp_no, emp_name, dept, manager) VALUES (2, 'Dinesh', 'sales', 'Bhoomi');
-INSERT INTO Employee (emp_no, emp_name, dept, manager) VALUES (3, 'Faizal', 'sales', 'Bhoomi');
-INSERT INTO Employee (emp_no, emp_name, dept, manager) VALUES (4, 'Varun', 'finance', 'Chandan');
-INSERT INTO Employee (emp_no, emp_name, dept, manager) VALUES (5, 'Chandan', 'finance', 'Chandan');
-INSERT INTO Employee (emp_no, emp_name, dept, manager) VALUES (6, 'Tuhin', 'finance', 'Bhoomi');
-INSERT INTO Employee (emp_no, emp_name, dept, manager) VALUES (7, 'Rohit', 'quality', 'Anjali');
-INSERT INTO Employee (emp_no, emp_name, dept, manager) VALUES (8, 'Divya', 'management', 'Chandan');
-INSERT INTO Employee (emp_no, emp_name, dept, manager) VALUES (9, 'Santosh', 'management', 'Santosh');
-INSERT INTO Employee (emp_no, emp_name, dept, manager) VALUES (10, 'Anjali', 'quality', 'Anjali');
-INSERT INTO Employee (emp_no, emp_name, dept, manager) VALUES (11, 'Gourab', 'quality', 'Santosh');
-INSERT INTO Employee (emp_no, emp_name, dept, manager) VALUES (12, 'Himangshu', 'quality', 'Santosh');
-INSERT INTO Employee (emp_no, emp_name, dept, manager) VALUES (13, 'Akshay', 'sales', 'Chandan');
-INSERT INTO Employee (emp_no, emp_name, dept, manager) VALUES (14, 'Bhoomi', 'sales', 'Bhoomi');
-INSERT INTO Employee (emp_no, emp_name, dept, manager) VALUES (15, 'Koushik', 'management', 'Anjali');
-INSERT INTO Employee (emp_no, emp_name, dept, manager) VALUES (16, 'Suman', 'management', 'Santosh');
-INSERT INTO Employee (emp_no, emp_name, dept, manager) VALUES (17, 'Nishant', 'management', 'Anjali');
+INSERT INTO Employee VALUES (1, 'Mainak', 'sales', 'Bhoomi');
+INSERT INTO Employee VALUES (2, 'Dinesh', 'sales', 'Bhoomi');
+INSERT INTO Employee VALUES (3, 'Faizal', 'sales', 'Bhoomi');
+INSERT INTO Employee VALUES (4, 'Varun', 'finance', 'Chandan');
+INSERT INTO Employee VALUES (5, 'Chandan', 'finance', 'Chandan');
+INSERT INTO Employee VALUES (6, 'Tuhin', 'finance', 'Bhoomi');
+INSERT INTO Employee VALUES (7, 'Rohit', 'quality', 'Anjali');
+INSERT INTO Employee VALUES (8, 'Divya', 'management', 'Chandan');
+INSERT INTO Employee VALUES (9, 'Santosh', 'management', 'Santosh');
+INSERT INTO Employee VALUES (10, 'Anjali', 'quality', 'Anjali');
+INSERT INTO Employee VALUES (11, 'Gourab', 'quality', 'Santosh');
+INSERT INTO Employee VALUES (12, 'Himangshu', 'quality', 'Santosh');
+INSERT INTO Employee VALUES (13, 'Akshay', 'sales', 'Chandan');
+INSERT INTO Employee VALUES (14, 'Bhoomi', 'sales', 'Bhoomi');
+INSERT INTO Employee VALUES (15, 'Koushik', 'management', 'Anjali');
+INSERT INTO Employee VALUES (16, 'Suman', 'management', 'Santosh');
+INSERT INTO Employee VALUES (17, 'Nishant', 'management', 'Anjali');
 
 SELECT * FROM Employee;
 
-INSERT INTO Project (proj_code, proj_name, start_date, end_date, proj_mgr) VALUES (1, 'hotel management', '25-JUN-2019', '30-SEP-2019', 'Bhoomi');
-INSERT INTO Project (proj_code, proj_name, start_date, end_date, proj_mgr) VALUES (2, 'e-commerce', '12-JUL-2016', '31-Jan-2017', 'Santosh');
-INSERT INTO Project (proj_code, proj_name, start_date, end_date, proj_mgr) VALUES (3, 'railway management', '17-Jan-2015', '26-SEP-2018', 'Anjali');
-INSERT INTO Project (proj_code, proj_name, start_date, end_date, proj_mgr) VALUES (4, 'parking allocation', '06-AUG-2020', '30-NOV-2020', 'Chandan');
+INSERT INTO Project VALUES (1, 'hotel management', '25-JUN-2019', '30-SEP-2019', 'Bhoomi');
+INSERT INTO Project VALUES (2, 'e-commerce', '12-JUL-2016', '31-Jan-2017', 'Santosh');
+INSERT INTO Project VALUES (3, 'railway management', '17-Jan-2015', '26-SEP-2018', 'Anjali');
+INSERT INTO Project VALUES (4, 'parking allocation', '06-AUG-2020', '30-NOV-2020', 'Chandan');
 
 SELECT * FROM Project;
 
-INSERT INTO WorksOn (emp_no, proj_code) VALUES (1, 1);
-INSERT INTO WorksOn (emp_no, proj_code) VALUES (2, 1);
-INSERT INTO WorksOn (emp_no, proj_code) VALUES (3, 1);
-INSERT INTO WorksOn (emp_no, proj_code) VALUES (4, 4);
-INSERT INTO WorksOn (emp_no, proj_code) VALUES (5, 4);
-INSERT INTO WorksOn (emp_no, proj_code) VALUES (6, 1);
-INSERT INTO WorksOn (emp_no, proj_code) VALUES (7, 3);
-INSERT INTO WorksOn (emp_no, proj_code) VALUES (8, 4);
-INSERT INTO WorksOn (emp_no, proj_code) VALUES (9, 2);
-INSERT INTO WorksOn (emp_no, proj_code) VALUES (10, 3);
-INSERT INTO WorksOn (emp_no, proj_code) VALUES (11, 2);
-INSERT INTO WorksOn (emp_no, proj_code) VALUES (12, 2);
-INSERT INTO WorksOn (emp_no, proj_code) VALUES (13, 4);
-INSERT INTO WorksOn (emp_no, proj_code) VALUES (14, 1);
-INSERT INTO WorksOn (emp_no, proj_code) VALUES (15, 3);
-INSERT INTO WorksOn (emp_no, proj_code) VALUES (16, 2);
-INSERT INTO WorksOn (emp_no, proj_code) VALUES (17, 3);
+INSERT INTO WorksOn VALUES (1, 1);
+INSERT INTO WorksOn VALUES (2, 1);
+INSERT INTO WorksOn VALUES (3, 1);
+INSERT INTO WorksOn VALUES (4, 4);
+INSERT INTO WorksOn VALUES (5, 4);
+INSERT INTO WorksOn VALUES (6, 1);
+INSERT INTO WorksOn VALUES (7, 3);
+INSERT INTO WorksOn VALUES (8, 4);
+INSERT INTO WorksOn VALUES (9, 2);
+INSERT INTO WorksOn VALUES (10, 3);
+INSERT INTO WorksOn VALUES (11, 2);
+INSERT INTO WorksOn VALUES (12, 2);
+INSERT INTO WorksOn VALUES (13, 4);
+INSERT INTO WorksOn VALUES (14, 1);
+INSERT INTO WorksOn VALUES (15, 3);
+INSERT INTO WorksOn VALUES (16, 2);
+INSERT INTO WorksOn VALUES (17, 3);
 
 SELECT * FROM WorksOn;
 
@@ -95,7 +95,9 @@ FROM Dept;
 -- Q2
 SELECT P.proj_name, P.proj_mgr, E1.emp_name, E2.dept
 FROM Project P, WorksOn W, Employee E1, Employee E2
-WHERE P.proj_code = W.proj_code AND W.emp_no = E1.emp_no AND P.proj_mgr = E2.emp_name
+WHERE P.proj_code = W.proj_code AND 
+      W.emp_no = E1.emp_no AND 
+      P.proj_mgr = E2.emp_name
 ORDER BY P.proj_name;
 
 -- Q3
