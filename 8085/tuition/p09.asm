231af6e424d954058bc528e1915124e42a954103
gourab-stu <gourab.acharjee2005@gmail.com>
2024-11-28 00:29:23 +0530

    replaced labels L with N (means NEXT)

--- 8085/tuition/p09.asm
+++ 8085/tuition/p09.asm
@@ -1,7 +1,7 @@
    MVI A, 23H    // [A] <- 23H (0010 0011), [Cy] = X
    MVI D, 00H    // [D] <- 00H
    RAL           // [A] <- (0100 011X), [Cy] = 0
-   JNC L         // JUMP TO L IF [Cy] = 0
+   JNC N         // JUMP TO N IF [Cy] = 0
    INR D
-L: HLT           // if [D] = 00H, then positive
+N: HLT           // if [D] = 00H, then positive
                  // if [D] = 01H, then negative
